32077825d08a1be247cebf967b93e1fcb11a51b4
Ivan Rodriguez <sbr.com@hotmail.com>
2023-03-15 11:02:41 -0600

    updating packages and exposing API

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github/ivanxtr/golang/api"
+	db "github/ivanxtr/golang/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start the server", err)
+	}
+}
